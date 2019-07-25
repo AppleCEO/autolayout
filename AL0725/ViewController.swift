aee982cd54c05d6160210eb6c98e523e87af1d9f
kiljoonho <kiljh@me.com>
2019-07-25 11:54:27 +0900

    Initial Commit

---
+++ AL0725/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AL0725
+//
+//  Created by joon-ho kil on 7/25/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
