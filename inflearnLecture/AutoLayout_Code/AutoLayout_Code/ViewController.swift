0c4f0e5c8cb8e639c241b16e3c0960aae086d7e3
도미닉 <kiljh@me.com>
2019-12-24 01:14:17 +0900

    Basic_22 finish

--- inflearnLecture/AutoLayout_Code/AutoLayout_Code/ViewController.swift
+++ inflearnLecture/AutoLayout_Code/AutoLayout_Code/ViewController.swift
@@ -21,6 +21,15 @@ class ViewController: UIViewController {
         myButton.translatesAutoresizingMaskIntoConstraints = false
         myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 100).isActive = true
+        
+        let myLabel = UILabel.init()
+        myLabel.text = "My Label"
+        
+        self.view.addSubview(myLabel)
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        myLabel.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
     }
 
 
