4b7e62c6c7200d66ba74d12a4014933935b65e5f
도미닉 <kiljh@me.com>
2019-12-24 01:09:38 +0900

    Basic_21 finish

---
+++ inflearnLecture/AutoLayout_Code/AutoLayout_Code/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AutoLayout_Code
+//
+//  Created by joon-ho kil on 2019/12/24.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myButton = UIButton.init(type: .system)
+        myButton.setTitle("My Button", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 100).isActive = true
+    }
+
+
+}
+
