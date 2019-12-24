cb1f78e6db852bd0154425c889ee35d64c3935ac
도미닉 <kiljh@me.com>
2019-12-24 14:29:46 +0900

    Custom_Keyboard_2 finish

---
+++ inflearnLecture/CustomKeyboard/CustomKeyboard/CustomKeyboard.swift
@@ -0,0 +1,14 @@
+//
+//  CustomKeyboard.swift
+//  CustomKeyboard
+//
+//  Created by joon-ho kil on 2019/12/24.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class CustomKeyboard: UIView {
+
+
+}
--- inflearnLecture/CustomKeyboard/CustomKeyboard/ViewController.swift
+++ inflearnLecture/CustomKeyboard/CustomKeyboard/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let loadNib = Bundle.main.loadNibNamed("CustomKeyboard", owner: nil, options: nil)
+        let myKeyboardView = loadNib?.first as! CustomKeyboard
+        firstTextField.inputView = myKeyboardView
+        
     }
 
 
