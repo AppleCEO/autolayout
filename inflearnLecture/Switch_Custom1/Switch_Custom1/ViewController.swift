515b5ab33d0ba399f262268f1129dd53c6279ace
도미닉 <kiljh@me.com>
2019-12-24 21:25:44 +0900

    Custom_Switch_1 finish

---
+++ inflearnLecture/Switch_Custom1/Switch_Custom1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Switch_Custom1
+//
+//  Created by joon-ho kil on 2019/12/24.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var SwitchBackground: UIView!
+    @IBOutlet weak var buttonCenterX: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func selectedButton(_ sender: Any) {
+        
+    }
+    
+}
+
