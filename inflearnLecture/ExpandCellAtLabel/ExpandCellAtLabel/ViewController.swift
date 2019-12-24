804357de43ec9d90b935d5b0054368065c33087a
도미닉 <kiljh@me.com>
2019-12-24 17:15:18 +0900

    Expand_Cell_2 finish

--- inflearnLecture/ExpandCellAtLabel/ExpandCellAtLabel/ViewController.swift
+++ inflearnLecture/ExpandCellAtLabel/ExpandCellAtLabel/ViewController.swift
@@ -14,28 +14,53 @@ class ExpandCell: UITableViewCell {
 }
 
 class ViewController: UIViewController {
+    struct ExpandDataModel {
+        var description: String
+        var isExpand: Bool
+    }
+    
+    var dataModels = [ExpandDataModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let textArray = ["short Text", "long long long,long long long,long long long,long long long,long long longlong long long,long long long,long long long,long long long,long long long",
+            "short Text","short Text",
+            "long long long,long long long,long long long,long long long,long long longlong long long,long long long,long long long,long long long,long long long"]
         
+        for value in textArray {
+            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
+        }
     }
 
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return dataModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "expandCell_ID", for: indexPath) as! ExpandCell
         
-        cell.descriptionLabel.text = "hi"
+        cell.descriptionLabel.text = dataModels[indexPath.row].description
+        
+        cell.selectionStyle = .none
+
+        if dataModels[indexPath.row].isExpand == true {
+            cell.descriptionLabel.numberOfLines = 0
+            
+            return cell
+        }
+        
+        cell.descriptionLabel.numberOfLines = 1
         
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataModels[indexPath.row].isExpand.toggle()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
 }
