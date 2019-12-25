a2917a00138066ac3da81b6c6ae39df31f3ca7d0
도미닉 <kiljh@me.com>
2019-12-25 20:06:30 +0900

    Chat_3 finish

--- inflearnLecture/Chat_App/Chat_App/ViewController.swift
+++ inflearnLecture/Chat_App/Chat_App/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        chatTableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "myCell")
+        chatTableView.register(UINib(nibName: "YourCell", bundle: nil), forCellReuseIdentifier: "yourCell")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
-
+    @objc func keyboardWillShow(noti: Notification) {
+        let notiInfo = noti.userInfo!
+        let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        let height = keyboardFrame.size.height
+        
+        inputViewBottomMargin.constant = height
+    }
+    
+    @objc func keyboardWillHide(noti: Notification) {
+        inputViewBottomMargin.constant = 0
+    }
+    
+    @IBAction func sendString(_ sender: Any) {
+        
+    }
+    
 }
 
