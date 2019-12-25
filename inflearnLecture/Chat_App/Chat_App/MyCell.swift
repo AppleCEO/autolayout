def4f4666bc3a00b2501aa24db655ffbda72b66f
도미닉 <kiljh@me.com>
2019-12-25 19:41:48 +0900

    Chat_2 finish

---
+++ inflearnLecture/Chat_App/Chat_App/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  Chat_App
+//
+//  Created by joon-ho kil on 2019/12/25.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ inflearnLecture/Chat_App/Chat_App/YourCell.swift
@@ -0,0 +1,24 @@
+//
+//  YourCell.swift
+//  Chat_App
+//
+//  Created by joon-ho kil on 2019/12/25.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class YourCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
