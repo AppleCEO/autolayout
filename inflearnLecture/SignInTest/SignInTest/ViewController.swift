4fd32fc6b29607db1ead0c75388c7cb5f464360a
도미닉 <kiljh@me.com>
2019-12-25 18:03:38 +0900

    SignIn_3 finish

--- inflearnLecture/SignInTest/SignInTest/ViewController.swift
+++ inflearnLecture/SignInTest/SignInTest/ViewController.swift
@@ -23,13 +23,32 @@ class ViewController: UIViewController {
 
     @objc func textFieldEdited(textField: UITextField) {
         if textField == emailTextField {
+            if isValidEmail(email: textField.text) {
+                emailError.isHidden = true
+                return
+            }
             
+            emailError.isHidden = false
         }
         
-        if passwordTextField == passwordTextField {
+        if textField == passwordTextField {
+            if textField.text?.count ?? 0 >= 8 {
+                passwordError.isHidden = true
+                return
+            }
             
+            passwordError.isHidden = false
         }
     }
 
+    func isValidEmail(email:String?) -> Bool {
+        
+        guard email != nil else { return false }
+        
+        let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let pred = NSPredicate(format:"SELF MATCHES %@", regEx)
+        return pred.evaluate(with: email)
+    }
 }
 
